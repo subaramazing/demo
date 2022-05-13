fa6a665a0016fe9fbb2690c46f4a203bce0a5e22
Admin <690438403@qq.com>
2022-05-13 21:24:12 +0800

    2-8 增加自定义配置项示例（默认先读配置文件，如果没有就读取默认值）

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,11 +1,9 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 /**
  * @author Admin
@@ -17,9 +15,12 @@ public class TestController {
 //    http://127.0.0.1:8880/hello
    //GET（查询）,POST（新增），put（修改），DELETE（删除）
    //RequestMapping支持以上所有的Mapping
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("/hello/post")
